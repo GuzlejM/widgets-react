ae5e2d96e0a71c956d096e6ea6c9bae197c69cc8
guzlej <martin.guzlej@gmail.com>
2021-03-18 06:21:50 +0100

    semantic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Accordion from './components/Accordion';
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end javascript library'
+    },
+    {
+        title: 'Second title',
+        content: 'Lorem ipsum is a placeholder text'
+    },
+    {
+        title: 'Third title',
+        content: 'Lorem ipsum may be used as'
+    }
+]
 
 export default () => {
-    return <h1> Widgets App</h1>
+    return (
+        <div>
+            <Accordion items={items}/>
+        </div>
+    );
 };
---
+++ src/components/Accordion.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Accordion = ({ items }) => {
+    const renderedItems = items.map(item => {
+        return <div>
+            <div className='title active' key={item.title}>
+                <i className='dropdwon icon'></i>
+                {item.title}
+            </div>
+            <div className='content active'>
+                <p>{item.content}</p>
+            </div>
+        </div>
+    });
+
+    return <div className='ui styled accordion'>{renderedItems}</div>
+};
+
+export default Accordion;
