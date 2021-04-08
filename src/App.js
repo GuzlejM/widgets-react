1ba05a1584876315d9a8be1ac96d27ccea4a2448
guzlej <martin.guzlej@gmail.com>
2021-04-08 09:47:34 +0200

    bug fixed typo in Route.js path instead pathname

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
 
 // "Database" items for Accordion which is sent as PROP
 const items = [
@@ -54,30 +55,39 @@ const showAccordion = () => {
   if (window.location.pathname === '/') {
     return <Accordion items={items} />;
   }
-}
+};
 const showList = () => {
   if (window.location.pathname === '/list') {
     return <Search />;
   }
-}
+};
 const showDropdown = () => {
   if (window.location.pathname === '/dropdown') {
     return <Dropdown />;
   }
-}
+};
 const showTranslate = () => {
   if (window.location.pathname === '/translate') {
     return <Translate />;
   }
-}
+};
+
 
   export default () => {
     return (
       <div>
-        {showAccordion()}
-        {showList()}
-        {showDropdown()}
-        {showTranslate()}
+        <Route path='/'>
+          <Accordion items={items} />
+        </Route>
+        <Route path='/list'>
+          <Search />
+        </Route>
+        <Route path='/dropdown'>
+          <Dropdown />
+        </Route>
+        <Route path='/translate'>
+          <Translate />
+        </Route>
       </div>
     );
   };
